163ad77498bd6fca9d11aa7361b45d0e0195947d
kiseikizm <85768664+kiseikizm@users.noreply.github.com>
2023-06-09 13:54:17 +0300

    PrimeNUM.py

--- PrimeNUM.py
+++ PrimeNUM.py
@@ -1,76 +1,3 @@
-
-# import numpy as np
-#
-# print("-----------------------------------")
-# arr=np.array([[1,2,3,4],[2,3,5,6],[5,4,3,2]])
-# for row in arr:
-#     for sayi in row:
-#         print(sayi, end=' ')
-#     print()
-# print("-----------------------------------")
-# square=np.square(arr)
-# for row in square:
-#     for item in row:
-#         print(item, end=' ')
-#     print()
-# print("-----------------------------------")
-# arrayim = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-# xd = arrayim.reshape(2, 5)
-#
-# for row in xd:
-#     for item in row:
-#         print(item, end=' ')
-#     print()
-# print("-----------------------------------")
-# random_arr = np.random.random((3, 3))
-# print(random_arr[0])
-# print(random_arr[1])
-# print(random_arr[2])
-# print("-----------------------------------")
-# arr = np.array([1, 2, 3, 4, 5])
-# print("Ortalama:", int(np.mean(arr)))  # Ortalama
-# print("Median:", int(np.median(arr)))  # Medyan
-# print("Maksimum:", np.max(arr))  # Maksimum
-# print("Minimum:", np.min(arr))  # Minimum
-# print("-----------------------------------")
-
-
-
-
-# print("------------------------------------------------------------------------")
-# def asal_sayilari_bul(ust_limit):
-#     asal_sayilar = []
-#     for num in range(2, ust_limit+1):
-#         if num > 1:
-#             for i in range(2, num):
-#                 if (num % i) == 0:
-#                     break
-#             else:
-#                 asal_sayilar.append(num)
-#     return asal_sayilar
-#
-# asal10000=(asal_sayilari_bul(10000))  # 10000'e kadar olan asal sayıları bulur
-#
-# print(asal10000)
-# print("------------------------------------------------------------------------")
-# print("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
-# def sieve_of_eratosthenes(n):
-#     prime = [True for i in range(n+1)]
-#     p = 2
-#     while (p * p <= n):
-#         if (prime[p] == True):
-#             for i in range(p * p, n+1, p):
-#                 prime[i] = False
-#         p += 1
-#     asal_sayilar = []
-#     for p in range(2, n):
-#         if prime[p]:
-#             asal_sayilar.append(p)
-#     return asal_sayilar
-#
-# print(sieve_of_eratosthenes(10000000))  # 10000000'e kadar olan asal sayıları bulur
-# print("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
-
 print("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
 import math
 
@@ -105,8 +32,5 @@ def segmented_sieve(n):
             for i in range(low, high + 1):
                 if sieve[i - low]:
                     f.write(str(i) + '\n')
-
-segmented_sieve(1000000000)  # 1.000.000.000'a kadar olan asal sayıları bulur ve 'prime_numbers.txt' dosyasına yazar
-
-
+segmented_sieve(1000000000)  # Finds prime numbers up to 1000000000 and writes to 'prime_numbers.txt'
 print("---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
